66648adf459549b734c9f0aed7613bc6f3bfaf4b
Akshat Gupta <akshat.gupta@hotelsuperhero.com>
2025-03-30 16:33:43 +0530

    minor fix

--- Backend/index.js
+++ Backend/index.js
@@ -1,32 +1,30 @@
-import express from "express";
-import dotenv from "dotenv";
-import connectToMongoDB from "./db/connectToMongoDB.js";
-import creatorRoutes from "./routes/creatorRoutes.routes.js";
-import studentRoutes from "./routes/studentRoutes.routes.js";
-import authRoutes from "./routes/authRoutes.routes.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
+import express from 'express';
+import dotenv from 'dotenv';
+import connectToMongoDB from './db/connectToMongoDB.js';
+import creatorRoutes from './routes/creatorRoutes.routes.js';
+import studentRoutes from './routes/studentRoutes.routes.js';
+import authRoutes from './routes/authRoutes.routes.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://focus-flow-lilac.vercel.app"],
-    credentials: true,
-  })
-);
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(cors({
+  origin: 'https://focus-flow-lilac.vercel.app',
+  credentials: true
+}));
+app.get('/', (req, res)  => {
+  res.send('Hello World');
+})
 
-app.use("/api/creator", creatorRoutes);
-app.use("/api/student", studentRoutes);
-app.use("/api/auth", authRoutes);
+app.use('/api/creator', creatorRoutes);
+app.use('/api/student', studentRoutes);
+app.use('/api/auth', authRoutes);
 
 app.listen(3000, () => {
   connectToMongoDB();
-  console.log("Server is running on port 3000");
-});
+  console.log('Server is running on port 3000');
+})
